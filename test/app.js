a521cba8358c4d7b2a446a0a133f86a13f9bc01b
OnesmusBett <onesmuskipchumba5@gmail.com>
2024-11-08 13:16:07 +0300

    update:text

--- test/app.js
+++ test/app.js
@@ -1,7 +1,15 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/test');
+const { Schema, connect, model }  = require("mongoose")
+const uri = "mongodb+srv://onesmuskipchumba5:InNsjjJAqTZgbXCY@cluster0.xj96h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=false";
+main().catch(err => console.log(err));
 
-const Cat = mongoose.model('Cat', { name: String });
-
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
+async function main() {
+  await connect(uri);
+  const kittySchema = new Schema({
+    name: String
+  });
+  const Kitten = model('Kitten', kittySchema);
+  const fluffy = new Kitten({ name: 'Fluffy' });
+  await fluffy.save();
+  console.log(fluffy);
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
